44d69440e8a472d24ee83527628143fcc20ecc32
kobolddoido <kobolddoido@gmail.com>
2013-10-25 22:48:41 -0200

    preparando para treinar, vetores prontos

--- dividindo_exons_treinamento.py
+++ dividindo_exons_treinamento.py
@@ -2,11 +2,13 @@ import re
 from Bio import Motif
 from Bio.Alphabet import IUPAC
 from Bio.Seq import Seq
+import simplehmm
 m = Motif.Motif(alphabet=IUPAC.unambiguous_dna)
 arquivo = open('introns_ciRNAs.txt','r').read()
 frequencias_5=[]
 frequencias_3=[]
 treinamento=[]
+entrada_treinamento=[]
 separando= re.split('>',arquivo)
 for seq in separando:
     if len(seq)>0:
@@ -36,39 +38,76 @@ for seq in separando:
         m.add_instance(Seq(seq[inicio-11:inicio],m.alphabet))
         #m.add_instance(Seq(seq[0:6],m.alphabet))
         onze=11
+        final=1
+        '''CHECAR OS ESTADOS FINAIS FINAL8'''
+        estados=['antes_branch','BRANCH','branch_11','branch_10','branch_9','branch_8','branch_7','branch_6','branch_5','branch_4','branch_3','branch_2','corpo','Final1','Final2','Final3','Final4','Final5','Final6','Final7','Final8']
+        emissoes=['A','C','T','G']
+        treinamento=[]
         for i in range(len(seq)):
-
+            debug=seq[i]
+            entrada_vetor=[]
             '''
             capturando a ponta ate  o branch
             '''
             if i > inicio:
-                print ponta,'ponta',seq[i],i
-                entrada_vetor.append('antes_branch')
+                #print ponta,'ponta',seq[i],i
+
                 entrada_vetor.append(seq[i])
+                entrada_vetor.append('antes_branch')
+                treinamento.append(entrada_vetor)
             '''
             capturando branch
             '''
             if i == inicio:
-                print ponta,'BRANCH',seq[i],i
-                entrada_vetor.append('BRANCH')
-                entrada_vetor.append(seq[i])
+                #print ponta,'BRANCH',seq[i],i
 
+                entrada_vetor.append(seq[i])
+                entrada_vetor.append('BRANCH')
+                treinamento.append(entrada_vetor)
+            '''
+            capturando_11 apos branch
+            '''
             if i < inicio and i > (inicio-11):
-                print ponta,'branch_'+str(onze),seq[i],i
+                #print ponta,'branch_'+str(onze),seq[i],i
+
+                entrada_vetor.append(seq[i])
                 entrada_vetor.append('branch_'+str(onze))
-                entrada_vetor.append(seq[i])
                 onze=onze-1
+                treinamento.append(entrada_vetor)
             if i <(inicio-11) and i>7:
-                print ponta,'corpo',seq[i],i
+                #print ponta,'corpo',seq[i],i
+
+                entrada_vetor.append(seq[i])
                 entrada_vetor.append('corpo')
-                entrada_vetor.append(seq[i])
+                treinamento.append(entrada_vetor)
             if i <= 7:
-                print ponta,'FINAL',seq[i],i
-                entrada_vetor.append('Final')
+               # print ponta,'FINAL'+str(final),seq[i],i
+
                 entrada_vetor.append(seq[i])
-        print entrada_vetor
-for x in  m.pwm():
-    print x
+                entrada_vetor.append('Final'+str(final))
+                final+=1
+                treinamento.append(entrada_vetor)
+            print entrada_vetor
+    if len(treinamento):
+        entrada_treinamento.append(treinamento)
+
+for y in entrada_treinamento:
+    print y
+
+
+
+
+
+#for y in entrada_treinamento:
+#    print y
+cirRNA_hmm= simplehmm.hmm('cirRNA_hmm_primeiro_teste',estados,emissoes)
+#teste=simplehmm.hmm()
+cirRNA_hmm.train(entrada_treinamento, smoothing='absdiscount')
+print cirRNA_hmm.print_hmm()
+#for imprimir in treinamento:
+#    print imprimir
+#for x in  m.pwm():
+#    print x
 m.weblogo('teste2.png')
     #print seq
 
